1520153f964757382056a8e46038b27faa41e393
Aaron Leung <aaron.leung@moovweb.com>
2015-02-28 00:09:39 -0800

    Rolling our own slice objects. Pretty much just copying Go's slices.

--- slice.cpp
+++ slice.cpp
@@ -1,10 +1,90 @@
+#include <sstream>
 #include "slice.hpp"
-
-#include <vector>
 
 namespace Vole {
 
-  using namespace std;
+  using std::stringstream;
 
+  template <typename T>
+  Slice<T>::Slice(size_t l, size_t c)
+  : mem(new T[c]),
+    beg(mem),
+    len(l),
+    cap(c)
+  { }
 
+  template <typename T>
+  Slice<T>::Slice(T* m, T* b, size_t l, size_t c)
+  : mem(m),
+    beg(b),
+    len(l),
+    cap(c)
+  { }
+
+  template <typename T>
+  template <typename Allocator>
+  Slice<T>::Slice(size_t l, size_t c, Allocator& a)
+  : mem(a.template alloc<T>(c)),
+    beg(mem),
+    len(l),
+    cap(c)
+  { }
+
+  template <typename T>
+  template <typename Allocator>
+  Slice<T>::Slice(size_t l, size_t c, Allocator* a)
+  : mem(a->template alloc<T>(c)),
+    beg(mem),
+    len(l),
+    cap(c)
+  { }
+
+  template <typename T>
+  T& Slice<T>::operator[](size_t i) {
+    if (!(i < len)) {
+      // TODO: use an error class with more metadata
+      throw "slice index out of bounds";
+    }
+    return beg[i];
+  }
+
+  template <typename T>
+  Slice<T> Slice<T>::slice(size_t start, size_t end) {
+    if (end > len || start > end) {
+      // TODO: use an error class with more metadata
+      throw "invalid range for slice";
+    }
+    return { mem, beg + start, end - beg, cap };
+  }
+
+  template <typename T>
+  T& Slice<T>::first() {
+    return (*this)[0];
+  }
+
+  template <typename T>
+  T& Slice<T>::last() {
+    return (*this)[len-1];
+  }
+
+  template <typename T>
+  Slice<T> Slice<T>::take(size_t n) {
+    return slice(0, n);
+  }
+
+  template <typename T>
+  Slice<T> Slice<T>::drop(size_t n) {
+    return slice(n, len);
+  }
+
+  template <typename T>
+  Slice<T>::operator string() {
+    stringstream ss;
+    ss << '(';
+    for (size_t i = 0; i < len; ++i) {
+      ss << (i ? " " : "") << beg[i];
+    }
+    ss << ')';
+    return ss.str();
+  }
 }
--- slice.hpp
+++ slice.hpp
@@ -1,36 +1,45 @@
 #ifndef VOLE_SLICE
 #define VOLE_SLICE
 
+#include <cstddef>
 #include <string>
-#include <vector>
 
 namespace Vole {
 
-  template<typename T>
-  class Slice {
-  public:
-    Slice(size_t cap = 0, std::vector<T> *cont = new std::vector<T>()) : start(0), end(cap), contents(cont) {
-      contents.reserve(cap);
-    }
-    Slice* push(T t) {
-      contents.push_back(t);
-      end++;
-      return new Slice(end, contents);
-    }
-    Slice* sub(size_t newstart, size_t newend) {
-      if (newend - newstart > end - start) {
-        // error?
-      } else {
-        std::vector<T> *newcontents = new std::vector<T>(contents.at(newstart), contents.at(newend));
-        return new Slice(newend - newstart, newcontents);
-      }
-    }
-    std::string string(); // use stringstream
-  private:
-    size_t start; 
-    size_t end;
-    std::vector<T> *contents;
+  using std::size_t;
+  using std::string;
+
+  template <typename T>
+  struct Slice {
+    T*     mem;
+    T*     beg;
+    size_t len;
+    size_t cap;
+
+    Slice(size_t l, size_t c);
+    Slice(T* m, T* b, size_t l, size_t c);
+    // We'll also use the default copy constructors and assignment operators.
+
+    template <typename Allocator>
+    Slice(size_t l, size_t c, Allocator& a);
+    template <typename Allocator>
+    Slice(size_t l, size_t c, Allocator* a);
+
+    T& operator[](size_t i);
+    Slice<T> slice(size_t start, size_t end); // s[start:end]
+
+    T& first();              // s[0]
+    T& last();               // s[len(s)-1]
+    Slice<T> take(size_t n); // s[:n]
+    Slice<T> drop(size_t n); // s[n:]
+
+    operator string();
   };
+
+  template <typename T>
+  Slice<T> append(Slice<T> s, T t);
+  template <typename T>
+  Slice<T> concat(Slice<T> s1, Slice<T> s2);
 
 }
 
