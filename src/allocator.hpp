e08974337ab16fc9f50dc22568bd7fb97ff03875
Aaron Leung <aaron.leung@moovweb.com>
2015-03-10 08:26:10 -0700

    include the header and use the namespaced versions of malloc/free

--- src/allocator.hpp
+++ src/allocator.hpp
@@ -1,6 +1,7 @@
 #ifndef VOLE_ALLOCATOR
 #define VOLE_ALLOCATOR
 
+#include <cstdlib>
 #include <forward_list>
 
 namespace Vole {
@@ -10,12 +11,12 @@ namespace Vole {
     
     public:
     ~Allocator() {
-      for (auto it : objects) free(it);
+      for (auto it : objects) std::free(it);
     }
 
     template <typename T>
     T* alloc(size_t c) {
-      void* ptr = malloc(c*sizeof(T));
+      void* ptr = std::malloc(c*sizeof(T));
       objects.push_front(ptr);
       return static_cast<T*>(ptr);
     }
