8520607a3377aae0304de6b0932b43b3fbe61f4b
Aaron Leung <aaron.leung@moovweb.com>
2015-03-12 22:46:21 -0700

    adding another test case

--- test/value_test.cpp
+++ test/value_test.cpp
@@ -20,4 +20,29 @@ int main() {
   auto things = Value(Vector(alloc, { thing1, thing2, thing3 }));
   cout << "things: " << things << endl;
 
+  auto boolval = Value(true);
+  auto numval = Value(2.78);
+  strval = Value(String(alloc, "a string"));
+  auto values = Value(Vector(alloc, { boolval, numval, strval }));
+  cout << "values: " << values << endl;
+
+  // (def (square x) (* x x))
+  auto def = Value(String(alloc, "def"));
+  auto square = Value(String(alloc, "square"));
+  auto x = Value(String(alloc, "x"));
+  auto mul = Value(String(alloc, "*"));
+
+  auto sig = Value(Vector(alloc, { square, x }));
+  auto body = Value(Vector(alloc, { mul, x, x }));
+  auto square_def = Value(Vector(alloc, { def, sig, body }));
+
+  cout << square_def << endl;
+
+  // (def (factorial n)
+  //   (if (= n 0)
+  //       1
+  //       (* n (factorial (- n 1)))))
+
+
 }
+
