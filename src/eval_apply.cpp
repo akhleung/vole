0995a58732485bc6534207bce5d76ae51c6d5a9f
Wonja Fairbrother <wfairbrother@gmail.com>
2015-04-06 17:56:50 -0700

    eval & apply

--- src/eval_apply.cpp
+++ src/eval_apply.cpp
@@ -19,12 +19,12 @@ namespace Vole {
       break;
 
       case Value::SYMBOL:
-        // ???
+        return env.lookup(expr);
       break;
 
       // compound expressions
       case Value::VECTOR:
-        // ???
+        return eval_combination(ctx, expr.content.vector, env)
       break;
 
       default:
@@ -34,11 +34,43 @@ namespace Vole {
   }
 
   Value eval_combination(Context& ctx, Vector expr, Env& env) {
+    Value id = expr.head();
+    Value quote = ctx.new_symbol("quote");
+    if (id.type == Value::SYMBOL && eq(id, quote)) {
+      return expr[1];
+    } else {
+      Value results = ctx.new_vector(expr.len);
+      for (int i = 0; i < expr.len; ++i) {
+        results = append(ctx.allocator, results, eval(ctx, expr[i], env));
+      }
+      if (results.head().type != Value::FUNCTION) {
+        throw "first argument is not a function";
+      }
+      return apply(ctx, results.head().content.function, results.tail());
+    }
 
+    // switch (id.type) {
+    //   case Value::SYMBOL:
+    //     if (id == quote) {
+    //       return expr[1];
+    //     }
+
+    //   break;
+    //  default:
+    //    Value results = ctx.new_vector(expr.len);
+    //    for (int i = 0; i < expr.len; ++i) {
+    //      results.append(eval(ctx, expr[i], env));
+    //    }
+    //    if (results[0].type != Value::FUNCTION) {
+    //      throw "first argument is not a function";
+    //    }
+    //    return apply(ctx, results[0], results[1]);
+    //  break;
+    // }
   }
 
   Value apply(Context& ctx, Function func, Vector args) {
-
+    return func(ctx, args);
   }
 
 }
